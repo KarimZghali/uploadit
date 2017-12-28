8b58dbc96609ec3f9844fca6560ed01cf01df282
Karim <32277834+KarimZghali@users.noreply.github.com>
2017-12-28 19:41:40 +0100

    Update CheckPictures.php

--- src/Check/CheckPictures.php
+++ src/Check/CheckPictures.php
@@ -5,9 +5,8 @@ use UPLOADIT\Model\AllocineModel;
 class CheckPictures
 {
     public static function check($bdc, $format, $mediaObj) {
-        var_dump($mediaObj->getWeight());
 
-
+        $errorUpload = null;
 
         $target_dir = "./advertising/";
      //   $target_file = $target_dir . basename($_FILES["file-Habillage-Pc"]["name"]);
@@ -34,51 +33,51 @@ class CheckPictures
         if(isset($_POST["upload"])) {
             $check = getimagesize($_FILES["file-".$format]["tmp_name"]);
             if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
+               // echo "File is an image - " . $check["mime"] . ".";
                 $uploadOk = 1;
             } else {
-                echo "File is not an image.";
+                $errorUpload = $errorUpload."Votre fichier n'est pas de type image.<br/>";
                 $uploadOk = 0;
             }
         }
 // Check if file already exists
         if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
+            $errorUpload = $errorUpload."Un visuel existe déjà pour cette campagne.<br/>";
             $uploadOk = 0;
         }
 
 // Check file size
         if ($_FILES["file-".$format]["size"] > $mediaObj->getWeight() * 100) {
             $gap = ($_FILES["file-".$format]["size"]/1000) - $mediaObj->getWeight();
-            echo "Sorry, your file is too large.".$gap." ko too much";
+            $errorUpload =  $errorUpload."Votre fichier est trop lourd, il dépasse de ".$gap."ko.<br>";
             $uploadOk = 0;
         }
 
         if ($width != $mediaObj->getWidth()) {
-            echo "La largeur de l'image doit etre de ".$mediaObj->getWidth()."px alors que votre visuel à une largeur de ".$width."px";
+            $errorUpload = $errorUpload."La largeur de l'image doit être de ".$mediaObj->getWidth()."px.<br/>";
             $uploadOk = 0;
         }
 
         if ($height != $mediaObj->getHeight()) {
-            echo "La hauteur de l'image doit etre de ".$mediaObj->getHeight()."px alors que votre visuel à une hauteur de ".$height."px";
+            $errorUpload = $errorUpload."La hauteur de l'image doit être de ".$mediaObj->getHeight()."px.<br/>";
             $uploadOk = 0;
         }
 
 // Allow certain file formats
         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
             && $imageFileType != "gif" ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $errorUpload = $errorUpload."Le ".$extension_upload." n'est pas accépté (jpeg, jpg, png, gif uniquement).<br/>";
             $uploadOk = 0;
         }
 // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
+            return $errorUpload;
 // if everything is ok, try to upload file
         } else {
             if (move_uploaded_file($_FILES["file-".$format]["tmp_name"], $target_dir . $file)) {
-                echo "The file ". basename( $_FILES["file-".$format]["name"]). " has been uploaded.";
+                return $errorUpload;
             } else {
-                echo "Sorry, there was an error uploading your file.";
+                return $errorUpload."Sorry, there was an error uploading your file.";
             }
         }
 
