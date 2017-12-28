11c48f034a873e5bea5d83a40380b0cc7d35427c
Karim <32277834+KarimZghali@users.noreply.github.com>
2017-12-28 17:10:49 +0100

    Update AllocineController.php

--- src/Controller/Media/AllocineController.php
+++ src/Controller/Media/AllocineController.php
@@ -1,20 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: silve
- * Date: 18/11/2017
- * Time: 12:13
- */
+
 
 namespace UPLOADIT\Controller\Media;
 use UPLOADIT\Model\AllocineModel;
-;use UPLOADIT\Controller\Controller;
+use UPLOADIT\Controller\Controller;
+use UPLOADIT\Check\CheckPictures;
 
 
 class AllocineController extends Controller
 {
 
-    public function manage($bdc, $media) {
+
+
+    public function manage($bdc, $media, $emailCustomer) {
 
         $entityManager = require_once join(DIRECTORY_SEPARATOR, [__DIR__, '/../../../bootstrap.php']);
 
@@ -24,11 +22,44 @@ class AllocineController extends Controller
 
         $model->checkBdcData($bdc, $entityManager);
 
-        $model->read($bdc, $entityManager);
+        // Si l'utilisateur renseigne une adresse mail:
+        if ($emailCustomer) {
+            $model-> addCustomer($bdc, $emailCustomer, $entityManager);
+        }
+
+        // Verification si l'img uploadé respecte bien les specifications techniques
+        // et upload si tout est ok
+        if (isset($_POST["upload"])) {
+
+            $allocineCheckPictures = new AllocineModel();
+            $allocineCheckPictures->read($bdc, $entityManager, $_POST["format"] );
+            CheckPictures::check($bdc, $_POST["format"], $allocineCheckPictures);
+        }
+
+        if (isset($_GET["format"])) {
+           if ($_GET["format"] == "Habillage-smartphone") {
+               $format = $_GET["format"];
+           } else if ($_GET["format"] == "Habillage-tablette") {
+               $format = $_GET["format"];
+           } else if ($_GET["format"] == "Demi-page") {
+               $format = $_GET["format"];
+           } else {
+               $format = "Habillage-Pc";
+           }
+        } else {
+            $format = "Habillage-Pc";
+        }
+
+        $model->read($bdc, $entityManager, $format);
 
         include(__DIR__."/../../../app/views/allocine/manage.php");
 
     }
 
+    public function boxView($formatBox, $model, $picture) {
+
+        return $model->box($formatBox, $picture);
+
+    }
 
 }
