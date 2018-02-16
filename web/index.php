b8c79533e1c61b2bcd853caf143c3d0efe7c6903
Karim <32277834+KarimZghali@users.noreply.github.com>
2018-02-16 17:59:10 +0100

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,51 +1,54 @@
-<!DOCTYPE html>
 <?php
 
 use UPLOADIT\Controller\Media\AllocineController;
 use UPLOADIT\Controller\Media\AdmooveController;
 use UPLOADIT\Controller\Media\NrjController;
 use UPLOADIT\Controller\Media\AuthBdcController;
+use UPLOADIT\Controller\Admin\AdminControler;
 
 require "./../vendor/autoload.php";
 
 try {
 
+    //recovering data from the URL
+    $bdc      = (string) filter_input(INPUT_GET, "bdc");
+    $media    = (string) filter_input(INPUT_GET, "media");
+    $failAuth = (boolean)filter_input(INPUT_GET, "failAuth");
 
-    $bdc = (string) filter_input(INPUT_GET, "bdc");
-    $media = (string) filter_input(INPUT_GET, "media");
-    $failAuth = (boolean) filter_input(INPUT_GET, "failAuth");
+    $emailCustomer = (string) filter_input(INPUT_POST, "email-customer");
 
-
+    //If the bdc is informed but not the media
     if (($bdc) && !($media)) {
         $failAuth = true;
     }
 
+    $route = [
+        "allocine" => AllocineController::class,
+        "admoove"  => AdmooveController::class,
+        "nrj"      => NrjController::class,
+        "admin"    => AdminControler::class,
+    ];
 
-        $route = [
-            "allocine" => AllocineController::class,
-            "admoove"  => AdmooveController::class,
-            "nrj"      => NrjController::class,
-           // "authBdc"  => AuthBdcController::class,
-        ];
+    //Searching for correspondence with URL data
+    foreach ($route as $routeValue => $className) {
 
-
-        foreach ($route as $routeValue => $className) {
-
-            if ($media == $routeValue) {
-                $controller = new $className;
-                $response = $controller->{'manage'}($bdc, $media);
-                break;
-            }
-
+        if ($media == $routeValue) {
+            $controller = new $className;
+            $response = $controller->{'manage'}($bdc, $media, $emailCustomer);
+            break;
         }
 
-        if (!isset($controller)) {
-            $controller = new AuthBdcController;
-            $response = $controller->manage($failAuth);
-        }
+    }
+
+    // if no correspondence with the URL data
+    if (!isset($controller)) {
+        $controller = new AuthBdcController;
+        $response = $controller->manage($failAuth);
+    }
 
 } catch(throwable $e) {
-    echo '<h1>ERROR : </h1>'.(string) $e;
+    header("HTTP/1.1 500 Internal Server Error");
+    echo '<h1>!ERROR : </h1>'.(string) $e;
 }
 
 ?>
