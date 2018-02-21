4c833213eeaf22b0bdcfc8f69b3b56c1298d4102
Cyril <consultant@seeren.fr>
2018-02-21 23:16:26 +0100

    Update some forms link to relative for run app

--- app/views/ui/formUpload.php
+++ app/views/ui/formUpload.php
@@ -2,7 +2,7 @@
 
 
 
-<form method="post" enctype="multipart/form-data" action="/uploadit/web/?bdc=<?php echo $_GET["bdc"]; ?>&media=<?php echo $_GET["media"]; ?>" class="form-file flex">
+<form method="post" enctype="multipart/form-data" action="?bdc=<?php echo $_GET["bdc"]; ?>&media=<?php echo $_GET["media"]; ?>" class="form-file flex">
     <input type='hidden' name='format' value='<?php echo $formatBox; ?>'>
     <input type="file" name="file-<?php echo $formatBox; ?>" required>
     <button type="submit" name="upload" value="">Envoyer</button>
